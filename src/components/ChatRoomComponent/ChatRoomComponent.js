56e573a69f4c786b29ca8da0fb1708d8c211c049
diego-mac <dijavaji@gmail.com>
2023-11-02 03:57:43 -0500

    creo componente chatroom consumo web socket

---
+++ src/components/ChatRoomComponent/ChatRoomComponent.js
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react';
+import Stomp from 'stompjs';
+import Sockjs from 'sockjs-client';
+
+//import ImageNoFound from "../../assets/png/avatar.png";
+import "./ChatRoomComponent.css";
+import IconButton from '@material-ui/core/Button';
+import { TextField, Typography, ListItemText, Avatar, List, ListItem,ListItemAvatar} from '@material-ui/core';
+
+const ChatRoomComponent = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [nickName, setNickName] = useState('');
+  const [stompCLient, setStompCLient] = useState(null);
+
+  useEffect(()=>{
+    const socket = new Sockjs('http://127.0.0.1:8081/ws');
+    const client = Stomp.over(socket);
+
+    client.connect({},()=>{
+      client.subscribe('/topic/messages', (message)=>{
+        const receivedMessages = JSON.parse(message.body);
+        setMessages((prevMessages)=> [...prevMessages, receivedMessages]);
+      });
+    });
+    setStompCLient(client);
+    return () =>{
+      client.disconnect();
+    };
+  },[]);
+
+  const handleNickNameChange = (e)=>{
+    console.log(e.target.value);
+    setNickName(e.target.value);
+  }
+
+  const handleMessageChange = (e)=>{
+    setMessage(e.target.value);
+  };
+
+  const sendMessage = () =>{
+    if(message.trim()){
+        const chatMessage = {
+          displayName:nickName,
+          text: message,
+        };
+        stompCLient.send('/app/chat', {}, JSON.stringify(chatMessage));
+        setMessage('');
+    }
+  };
+
+  return (
+   <div>
+     <List>
+       {messages.map((msg, index) => (
+         <ListItem key={index}>
+           <ListItemAvatar>
+             <Avatar>{msg.displayName.charAt(0)}</Avatar>
+           </ListItemAvatar>
+           <ListItemText
+             primary={
+               <Typography variant="subtitle1">{msg.displayName}</Typography>
+             }
+             secondary={msg.text}
+           />
+         </ListItem>
+       ))}
+     </List>
+
+     <div style={{ display: 'flex', alignItems: 'center' }}>
+       <TextField
+         placeholder="Enter your nickname"
+         value={nickName}
+         onChange={handleNickNameChange}
+         autoFocus
+       />
+       <TextField
+         placeholder="Type a message"
+         value={message}
+         onChange={handleMessageChange}
+         fullWidth
+       />
+       <IconButton onClick={sendMessage} disabled={!message.trim()}>
+         send
+       </IconButton>
+     </div>
+   </div>
+ );
+}
+
+export default ChatRoomComponent;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.css";
+import ChatRoomComponent from "../../components/ChatRoomComponent";
+
+export default function Home() {
+  console.log("en home");
+  return (
+    <div>
+      <ChatRoomComponent/>
+    </div>
+  )
+}
