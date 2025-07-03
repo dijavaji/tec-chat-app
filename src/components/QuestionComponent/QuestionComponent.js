9859b20295840e55b461c3adf6b291e68a4d48cd
diegomac <dijavaji@gmail.com>
2025-07-02 20:38:47 -0500

    agrego componentes de preguntas respuesta

--- src/components/QuestionComponent/QuestionComponent.js
+++ src/components/QuestionComponent/QuestionComponent.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
 
-const QuestionComponent = () => {
+const QuestionSchema = Yup.object().shape({
+  text: Yup.string().required("Requerido"),
+});
+
+const QuestionComponent = ({ initial, onSubmit, onCancel }) => {
   return (
-    <div>MyComponent</div>
+    <div style={{ border: "1px solid #ddd", padding: 8, marginBottom: 8 }}>
+      <Formik
+        initialValues={{ text: initial?.text || "" }}
+        validationSchema={QuestionSchema}
+        onSubmit={async (values, { setSubmitting }) => {
+          await onSubmit(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label>Pregunta</label>
+              <Field name="text" />
+              <ErrorMessage name="text" component="div" style={{ color: "red" }} />
+            </div>
+            <div>
+              <button type="submit" disabled={isSubmitting}>
+                Guardar pregunta
+              </button>
+              <button type="button" onClick={onCancel}>
+                Cancelar
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
   )
 }
 
