f2c856f8b717b4acc317e04aa61bc0cb67a29b4a
diegomac <dijavaji@gmail.com>
2025-10-14 20:45:38 -0500

    agrego cerrado de opciones de perfil

--- src/components/ProfileComponent/SettingsComponent/SettingsComponent.js
+++ src/components/ProfileComponent/SettingsComponent/SettingsComponent.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import { useHistory } from "react-router-dom";
 
 import './SettingsComponent.css';
 
 const SettingsComponent = (props) => {
   const {setShowModal}=props;
+  const {logout} = useAuth();
+  const history = useHistory();
+
+  const onLogout = () =>{
+    //TODO cerrar la sesion en el backend
+    logout();
+    history.push('/');
+  }
+
   return (
     <div className='settings-form'>
       <button>Cambiar contrase&#241;a</button>
       <button>Cambiar email</button>
       <button>Descripci&#243;n</button>
-      <button>Cerrar sesi&#243;n</button>
+      <button onClick={onLogout} >Cerrar sesi&#243;n</button>
       <button onClick={() => setShowModal(false)}>Cancelar</button>
     </div>
   )
