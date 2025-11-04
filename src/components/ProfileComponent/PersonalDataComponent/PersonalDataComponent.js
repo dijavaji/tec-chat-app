c8a241f4161d6439301c19d1ddb2bae0dc92a48a
diegomac <dijavaji@gmail.com>
2025-11-04 17:19:19 -0500

    creacion de componente de datos personales

---
+++ src/components/ProfileComponent/PersonalDataComponent/PersonalDataComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './PersonalDataComponent.css'
+
+const PersonalDataComponent = () => {
+  return (
+    <div>MyComponent</div>
+  )
+}
+
+export default PersonalDataComponent
