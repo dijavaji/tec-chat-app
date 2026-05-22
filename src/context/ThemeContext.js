a30d0aa6fe3eee1730e0bc485fcb8069f292fc7c
dv-mac <dijavaji@gmail.com>
2026-05-22 08:06:48 -0500

    agrego landing temporal
    Co-Authored-By: djvasquezj <djvasquezj@uce.edu.ec>

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -5,7 +5,7 @@ const ThemeContext = createContext();
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem('theme');
-    return savedTheme || 'dark';
+    return savedTheme || 'light';
   });
 
   useEffect(() => {
